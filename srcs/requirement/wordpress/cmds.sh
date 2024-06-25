d1cb3eeaf03cf8096c27db40c6e7ba6550d194a9
omar-xy <omaromarhind@gmail.com>
2024-06-25 03:21:03 +0000

    change dockecompose and add clean script

--- srcs/requirement/wordpress/cmds.sh
+++ srcs/requirement/wordpress/cmds.sh
@@ -7,6 +7,7 @@ rm -rf *
 curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar /bin/wp
  
 wp core download --allow-root
+touch wp-config.ph && chmod +x wp-config.php
 cp wp-config-sample.php wp-config.php
 
 
@@ -22,7 +23,7 @@ chmod 600 wp-config.php
 
 chown -R www-data:www-data /var/www/html
 
-wp core install --url=localhost  --title=wordpress --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+wp core install --url=10.13.100.78  --title=wordpress --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
 
 wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
 
