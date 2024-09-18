1c66f09cb1dd6e9e22727f393f61f643f1659b04
Omar Taraki <otaraki@student.1337.ma>
2024-09-18 02:11:00 +0100

    done with the static website

--- srcs/requirement/bonus/ftp/tools/init-ftp.sh
+++ srcs/requirement/bonus/ftp/tools/init-ftp.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-FTP_CONF_FILE="/etc/vsftpd.conf"
-SCRIPT_FILE="/usr/local/bin/init-ftp.sh"
-
 sleep 15
 service vsftpd start
 
@@ -23,15 +19,4 @@ chown -R $ftp_user:$ftp_user /var/www/html
 echo $ftp_user | tee -a /etc/vsftpd.userlist &> /dev/null
 adduser ${ftp_user} root > /tmp/tst
 
-#you can entre the config file and cutsomize as wanted
-sed -i "s|#chroot_local_user=YES|chroot_local_user=YES|g"  $FTP_CONF_FILE && \
-sed -i "s|#local_enable=YES|local_enable=YES|g"  $FTP_CONF_FILE && \
-sed -i "s|#write_enable=YES|write_enable=YES|g"  $FTP_CONF_FILE && \
-sed -i "s|#local_umask=022|local_umask=007|g"  $FTP_CONF_FILE
-
-echo "allow_writeable_chroot=YES" >> $FTP_CONF_FILE && \
-echo 'seccomp_sandbox=NO' >> $FTP_CONF_FILE && \
-echo 'pasv_enable=YES' >> $FTP_CONF_FILE
-
-service vsftpd stop
 /usr/sbin/vsftpd
