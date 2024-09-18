4acf83b268fe8d56f1228c94745033fd2d4d1047
Omar Taraki <otaraki@student.1337.ma>
2024-09-18 17:12:39 +0100

    done with the static website

--- srcs/requirement/bonus/ftp/tools/init-ftp.sh
+++ srcs/requirement/bonus/ftp/tools/init-ftp.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 sleep 15
-service vsftpd start
 
-cat<<EOF > cr
+adduser $ftp_user --disabled-password
 
+echo "$ftp_user:$ftp_pwd" | chpasswd
 
+# chown -R "$ftp_user:$ftp_user" /var/www/html
 
-
-
-y
-EOF
-
-#set up FTP-USER
-adduser --home /var/www/html $ftp_user --disabled-password < cr
-rm cr
-echo "${ftp_user}:${ftp_pwd}" | /usr/sbin/chpasswd
-chown -R $ftp_user:$ftp_user /var/www/html
-echo $ftp_user | tee -a /etc/vsftpd.userlist &> /dev/null
-adduser ${ftp_user} root > /tmp/tst
-
-/usr/sbin/vsftpd
+vsftpd /etc/vsftpd.conf
